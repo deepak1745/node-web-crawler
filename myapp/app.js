42132cf58fb0dc3d371e1a3546ae7856788b5910
deepak.kumar10 <deepak.kumar10@olacabs.com>
2019-02-19 08:32:24 +0530

    final changes

--- myapp/app.js
+++ myapp/app.js
@@ -34,20 +34,24 @@ function visitPage(url, callback) {
   // Add page to our set
   pagesVisited[url] = true;
   numPagesVisited++;
+  console.log("Page: " + numPagesVisited);
 
   // Make the request
   console.log("Visiting page " + url);
+  console.log("\n");
   request(url, function(error, response, body) {
      // Check status code (200 is HTTP OK)
      console.log("Status code: " + response.statusCode);
+     console.log("\n");
      if(response.statusCode !== 200) {
        callback();
        return;
      }
      // Parse the document body
      var $ = cheerio.load(body);
-       console.log("----------- Extracting image links ------------");
-       collectImageLinks($);
+       console.log("----------- Extracting static links ------------");
+       console.log("\n");
+       collectStaticLinks($);
        console.log("\n");
        console.log("----------- Extracting external links ------------");
        collectExternalLinks($);
@@ -62,18 +66,32 @@ function visitPage(url, callback) {
 
 function collectInternalLinks($) {
     var relativeLinks = $("a[href^='https://wiprodigital.com/']");
-    console.log("Found " + relativeLinks.length + " relative links on page");
+    console.log("Found " + relativeLinks.length + " relative links on page:");
     relativeLinks.each(function() {
       console.log($(this).attr('href'));
         pagesToVisit.push($(this).attr('href'));
     });
 }
 
-function collectImageLinks($) {
+function collectStaticLinks($) {
     var imageLinks = $("img");
-    console.log("Found " + imageLinks.length + " image links on page");
+    console.log("Found " + imageLinks.length + " image links tags on page:");
     imageLinks.each(function() {
         console.log($(this).attr('src'));
+    });
+    console.log("\n");
+    var cssLinks = $("link");
+    console.log("Found " + cssLinks.length + " css links tags on page:");
+    cssLinks.each(function() {
+        console.log($(this).attr('href'));
+    });
+    console.log("\n");
+    var scriptLinks = $("script");
+    console.log("Found " + scriptLinks.length + " script tags (with or without links) on page:");
+    scriptLinks.each(function() {
+        if($(this).attr('src')){
+          console.log($(this).attr('src'));
+        }
     });
 }
 
